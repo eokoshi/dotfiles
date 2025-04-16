f7e5bac6aaed6ea743eeeec84376897900272dc6
eno:office <ethanokoshi+gh at gmail dot com>
2025-04-16 16:43:37 +0900

    asdf

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -7,12 +7,6 @@ return {
 	"AstroNvim/astrocommunity",
 	-- { import = "astrocommunity.pack.lua" },
 	-- { import = "astrocommunity.pack.python" },
-	{ import = "astrocommunity.motion.nvim-surround" },
-	{ import = "astrocommunity.git.gitgraph-nvim" },
-	{ import = "astrocommunity.colorscheme.catppuccin" },
-	{ import = "astrocommunity.colorscheme.bluloco-nvim" },
-	{ import = "astrocommunity.colorscheme.onedarkpro-nvim" },
-	{ import = "astrocommunity.colorscheme.gruvbox-nvim" },
 	-- { import = "astrocommunity.utility.hover-nvim" },
 	-- import/override with your plugins folder
 }
---
+++ dot_config/nvim/lua/plugins/colorthemes.lua
@@ -0,0 +1,96 @@
+return {
+	{
+		"uloco/bluloco.nvim",
+		lazy = true,
+		dependencies = { "rktjmp/lush.nvim" },
+		opts = {},
+	},
+	{
+		"olimorris/onedarkpro.nvim",
+		lazy = false,
+		opts = {
+			options = {
+				highlight_inactive_windows = true,
+			},
+		},
+	},
+	{
+		"sainnhe/everforest",
+		lazy = true,
+	},
+	{ "luisiacc/gruvbox-baby", lazy = true },
+	{ "ellisonleao/gruvbox.nvim", lazy = true },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"sainnhe/sonokai",
+		lazy = true,
+		dependencies = {
+			"AstroNvim/astrocore",
+			opts = {
+				options = {
+					g = {
+						sonokai_dim_inactive_windows = 1,
+					},
+				},
+			},
+		},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = true,
+		---@type CatppuccinOptions
+		opts = {
+			integrations = {
+				aerial = true,
+				alpha = true,
+				cmp = true,
+				dap = true,
+				dap_ui = true,
+				gitsigns = true,
+				illuminate = true,
+				indent_blankline = true,
+				markdown = true,
+				mason = true,
+				native_lsp = { enabled = true },
+				neotree = true,
+				notify = true,
+				semantic_tokens = true,
+				symbols_outline = true,
+				telescope = true,
+				treesitter = true,
+				ts_rainbow = false,
+				ufo = true,
+				which_key = true,
+				window_picker = true,
+				colorful_winsep = { enabled = true, color = "lavender" },
+			},
+		},
+		specs = {
+			{
+				"akinsho/bufferline.nvim",
+				optional = true,
+				opts = function(_, opts)
+					return require("astrocore").extend_tbl(opts, {
+						highlights = require("catppuccin.groups.integrations.bufferline").get(),
+					})
+				end,
+			},
+			{
+				"nvim-telescope/telescope.nvim",
+				optional = true,
+				opts = {
+					highlight = {
+						enable = true,
+						additional_vim_regex_highlighting = false,
+					},
+				},
+			},
+		},
+	},
+}
