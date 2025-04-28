ca62c220a3310a66dae5e4baae51a843e9005669
eno:office <ethanokoshi+code atgmail dot com>
2025-04-28 14:10:56 +0900

    Update .config/nvim/lua/plugins/treesitter.lua

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,24 +2,35 @@
 
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      "python",
-      "bash",
-      "markdown",
-      "markdown_inline",
-      "json",
-      "jsonc",
-      "ssh_config",
-      "sql",
-      "toml",
-      "r",
-      "git_config",
-      "dockerfile",
-      -- add more arguments for adding more treesitter parsers
-    },
-  },
+	"nvim-treesitter/nvim-treesitter",
+	opts = {
+		ensure_installed = {
+			"lua",
+			"vim",
+			"python",
+			"bash",
+			"markdown",
+			"markdown_inline",
+			"json",
+			"jsonc",
+			"ssh_config",
+			"sql",
+			"toml",
+			"r",
+			"git_config",
+			"dockerfile",
+			-- add more arguments for adding more treesitter parsers
+		},
+		highlights = {
+			disable = {
+				"csv",
+				"tsv",
+				"csv_semicolon",
+				"csv_whitespace",
+				"csv_pipe",
+				"rfc_csv",
+				"rfc_semicolon",
+			},
+		},
+	},
 }
