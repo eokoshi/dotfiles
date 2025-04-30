435ebe96787ceabde440c29c2689f2f3804b7c7d
eno:eeftop <ethanokoshi+code atgmail dot com>
2025-04-30 09:59:11 +0900

    Update .config/nvim/lua/plugins/jupytext.lua

--- dot_config/nvim/lua/plugins/jupytext.lua
+++ dot_config/nvim/lua/plugins/jupytext.lua
@@ -1,6 +1,8 @@
 return {
 	"GCBallesteros/jupytext.nvim",
-	config = true,
+	config = {
+		style = "percent",
+	},
 	-- Depending on your nvim distro or config you may need to make the loading not lazy
 	-- lazy=false,
 }
