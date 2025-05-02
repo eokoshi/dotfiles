5ff1dc50022aaab7212983ede61df974b597fed3
eno:eeftop <ethanokoshi+code atgmail dot com>
2025-05-02 14:51:42 +0900

    Update .config/nvim/lua/plugins/colorthemes.lua

--- dot_config/nvim/lua/plugins/colorthemes.lua
+++ dot_config/nvim/lua/plugins/colorthemes.lua
@@ -10,19 +10,25 @@ return {
 		lazy = true,
 		opts = {
 			options = {
+				cursorline = true,
 				highlight_inactive_windows = true,
 			},
 		},
 	},
 	{
 		"sainnhe/everforest",
-		lazy = true,
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.everforest_background = "hard"
+			vim.g.everforest_better_performance = 1
+			vim.g.everforest_enable_italic = 1
+		end,
 	},
-	{ "luisiacc/gruvbox-baby",    lazy = true },
 	{ "ellisonleao/gruvbox.nvim", lazy = true },
 	{
 		"folke/tokyonight.nvim",
-		lazy = false,
+		lazy = true,
 		priority = 1000,
 		opts = {},
 	},
@@ -35,59 +41,6 @@ return {
 				options = {
 					g = {
 						sonokai_dim_inactive_windows = 1,
-					},
-				},
-			},
-		},
-	},
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		lazy = true,
-		---@type CatppuccinOptions
-		opts = {
-			integrations = {
-				aerial = true,
-				alpha = true,
-				cmp = true,
-				dap = true,
-				dap_ui = true,
-				gitsigns = true,
-				illuminate = true,
-				indent_blankline = true,
-				markdown = true,
-				mason = true,
-				native_lsp = { enabled = true },
-				neotree = true,
-				notify = true,
-				semantic_tokens = true,
-				symbols_outline = true,
-				telescope = true,
-				treesitter = true,
-				ts_rainbow = false,
-				ufo = true,
-				which_key = true,
-				window_picker = true,
-				colorful_winsep = { enabled = true, color = "lavender" },
-			},
-		},
-		specs = {
-			{
-				"akinsho/bufferline.nvim",
-				optional = true,
-				opts = function(_, opts)
-					return require("astrocore").extend_tbl(opts, {
-						highlights = require("catppuccin.groups.integrations.bufferline").get(),
-					})
-				end,
-			},
-			{
-				"nvim-telescope/telescope.nvim",
-				optional = true,
-				opts = {
-					highlight = {
-						enable = true,
-						additional_vim_regex_highlighting = false,
 					},
 				},
 			},
