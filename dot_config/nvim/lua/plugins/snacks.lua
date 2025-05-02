942f4934b8a5afbd5d57f9c1e91b6b86d3081167
eno:eeftop <ethanokoshi+code atgmail dot com>
2025-05-02 14:49:47 +0900

    Add .config/nvim/lua/plugins/snacks.lua

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,54 @@
+return {
+	"folke/snacks.nvim",
+	lazy = false,
+	priority = 1000,
+	---@type snacks.Config
+	opts = {
+		dashboard = {
+			preset = {
+-- stylua: ignore start
+				header = [[
+  ,-.       _,---._ __  / \
+ /  )    .-'       `./ /   \
+ (  (   ,'            `/    /|
+  \  `-"             \'\   / |
+   `.              ,  \ \ /  |
+    /`.          ,'-`----Y   |
+   (            ;        |   '
+  |  ,-.    ,-'         |  /
+  |  | (   |            | /
+ )  |  \  `.___________|/
+.  `--'   `--'               .]],
+				-- stylua: ignore end
+			},
+			sections = {
+				{ section = "header" },
+				{
+					pane = 2,
+					section = "terminal",
+					cmd = "fortune",
+					height = 5,
+					padding = 1,
+				},
+				{ section = "keys", gap = 1, padding = 1 },
+				{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+				{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				{
+					pane = 2,
+					icon = " ",
+					title = "Git Status",
+					section = "terminal",
+					enabled = function()
+						return require("snacks").git.get_root() ~= nil
+					end,
+					cmd = "git status --short --branch --renames",
+					height = 5,
+					padding = 1,
+					ttl = 5 * 60,
+					indent = 3,
+				},
+				{ section = "startup" },
+			},
+		},
+	},
+}
