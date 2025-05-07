b1275991e474bd6e45fef0a59c9dabec656eec16
eno:office <ethanokoshi+code atgmail dot com>
2025-05-07 10:55:41 +0900

    Update .config/nvim/lua/config/autocmds.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -50,7 +50,7 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "gitcommit", "markdown", "text", "log" },
 	callback = function()
 		vim.opt_local.wrap = true
-		vim.opt_local.spell = true
+		-- vim.opt_local.spell = true
 	end,
 	group = general,
 	desc = "Enable Wrap in these filetypes",
