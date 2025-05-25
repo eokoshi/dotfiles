bf64894a98254b24c8ceef06e9d42eeaedaa8ad7
eno:eeftop <ethanokoshi+code atgmail dot com>
2025-05-25 12:01:59 +0900

    Add .config/nvim/lua/plugins/smear-cursor.lua

---
+++ dot_config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,10 @@
+--type @LazySpec
+return {
+	"sphamba/smear-cursor.nvim",
+	opts = {
+		smear_insert_mode = false,
+		legacy_computing_symbols_support = true,
+		legacy_computing_symbols_support_vertical_bars = true,
+		cursor_color = "#e2ad00",
+	},
+}
