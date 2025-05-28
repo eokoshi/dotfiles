67dcc408f4a5fe4db554434eae5ce72be2abc8b9
eno:office <ethanokoshi+code atgmail dot com>
2025-05-28 15:45:10 +0900

    asdf

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+# GitHub
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+gh auth login
+
+## Chezmoi
+sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
+chezmoi init --apply eokoshi
+
+## ripgrep and fd
+sudo apt-get -y --ignore-missing install ripgrep fd-find
+ln -s $(which fdfind) ~/.local/bin/fd
+
+## uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv python install
+
+## neovim (for x86 64 architecture)
+sudo rm -rf /opt/nvim
+sudo mkdir /opt/nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo tar -C /opt/nvim -xzf nvim-linux-x86_64.tar.gz --strip-components=1
+
+## lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+sudo rm -rf lazygit
+sudo rm lazygit.tar.gz
