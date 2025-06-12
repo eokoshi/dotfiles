bfb1ddd7287e225da0a77289a3d1fd8a86eba913
eno:DF2 <ethanokoshi+code atgmail dot com>
2025-06-12 18:41:07 +0900

    Add .config/nvim/lua/plugins/oil.lua.lua

---
+++ dot_config/nvim/lua/plugins/oil.lua.lua
@@ -0,0 +1,13 @@
+-- stylua: ignore
+-- if true then return {} end
+
+return {
+	'stevearc/oil.nvim',
+	dependencies = {
+		{ "echasnovski/mini.icons", opts = {} }
+	},
+	lazy = false,
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {},
+}
