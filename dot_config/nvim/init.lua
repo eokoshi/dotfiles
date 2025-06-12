90100fddb55693253b9250e5af0ecae6c34e7d7f
eno:DF2 <ethanokoshi+code atgmail dot com>
2025-06-12 18:45:18 +0900

    update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,17 +1,14 @@
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.maplocalleader = "\\"
 
 if vim.g.neovide then
 	-- Put anything you want to happen only in Neovide here
 	require("config.neovide")
 end
 
-require("config.lazy")
-
-vim.cmd.colorscheme("gruvbox-material")
-
 require("config.options")
 require("config.autocmds")
 require("config.mappings")
-require("config.highlights")
+require("config.lazy")
+require("config.colors")
 require("config.final")
--- dot_config/nvim/lua/config/colors.lua
+++ dot_config/nvim/lua/config/colors.lua
@@ -1,3 +1,5 @@
+vim.cmd.colorscheme("gruvbox-material")
+
 -- vim.api.nvim_set_hl(0, "RenderMarkdownCode", { bg = "NONE", fg = "NONE" })
 vim.api.nvim_set_hl(0, "LspSignatureActiveParameter", { italic = true, bold = true })
 vim.api.nvim_set_hl(0, "SnacksPickerPathIgnored", { link = "Ignore" })
