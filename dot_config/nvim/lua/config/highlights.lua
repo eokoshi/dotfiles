0f0f601fcdb7409540d537c1cc89bd308f82cbcf
eno:office <ethanokoshi+code atgmail dot com>
2025-06-25 17:27:30 +0900

    asdfasdf

--- dot_config/nvim/lua/config/highlights.lua
+++
@@ -1,4 +0,0 @@
--- vim.api.nvim_set_hl(0, "RenderMarkdownCode", { bg = "NONE", fg = "NONE" })
-vim.api.nvim_set_hl(0, "LspSignatureActiveParameter", { italic = true, bold = true })
-vim.api.nvim_set_hl(0, "SnacksPickerPathIgnored", { link = "Ignore" })
-vim.api.nvim_set_hl(0, "SnacksPickerGitStatussdfIgnored", { link = "Ignore" })
