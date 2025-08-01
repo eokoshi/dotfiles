2643216994688368d5cd97d16637364b51aaba62
eno:DF2 <ethanokoshi+code atgmail dot com>
2025-08-01 14:31:19 +0900

    Add .config/nvim/lua/plugins/smart-splits.lua Add .config/nvim/lua/plugins/tmux-nvim.lua

---
+++ dot_config/nvim/lua/plugins/smart-splits.lua
@@ -0,0 +1,12 @@
+if true then
+	return {}
+end
+
+return {
+	"mrjones2014/smart-splits.nvim",
+	event = "VeryLazy",
+	opts = {
+		ignored_filetypes = { "nofile", "quickfix", "qf", "prompt" },
+		ignored_buftypes = { "nofile" },
+	},
+}
---
+++ dot_config/nvim/lua/plugins/tmux-nvim.lua
@@ -0,0 +1,8 @@
+-- if true then return {}
+
+-- if having annoying clipboard behavior, it might be this plugin
+return {
+	"aserowy/tmux.nvim",
+	event = "VeryLazy",
+	opts = {},
+}
