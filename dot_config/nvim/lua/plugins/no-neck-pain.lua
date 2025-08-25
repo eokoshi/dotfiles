69aaee2ebb593f53a7613e6ffa07b2b2149c27a8
eokoshi <127910259+eokoshi@users.noreply.github.com>
2025-08-25 09:20:23 +0900

    Add .config/nvim/lua/plugins/diffview.lua Add .config/nvim/lua/plugins/no-neck-pain.lua Update .config/nvim/lua/plugins/org-list.lua

---
+++ dot_config/nvim/lua/plugins/no-neck-pain.lua
@@ -0,0 +1,12 @@
+-- if true then return {} end
+
+return {
+	"shortcuts/no-neck-pain.nvim",
+	version = "*",
+	opts = {
+		width = 130,
+		-- autocmds = {
+		-- 	skipEnteringNoNeckPainBuffer = true,
+		-- },
+	},
+}
