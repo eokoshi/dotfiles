8a7f2fbedf2208edc9cea9e06daa444d5c3d1bf7
eokoshi <127910259+eokoshi@users.noreply.github.com>
2025-09-02 14:45:40 +0900

    Update .config/nvim/lua/config/mappings.lua Update .config/nvim/lua/plugins/highlight-colors.lua

--- dot_config/nvim/lua/config/mappings.lua
+++ dot_config/nvim/lua/config/mappings.lua
@@ -267,6 +267,7 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	pattern = "*.py",
 	callback = function()
 		map("n", "<Leader>fi", "/import<CR>", { desc = "Jump to imports", buffer = true})
+		map("v", "gd", ":norm ysaw'f=r:A,<CR>gv<Plug>(nvim-surround-visual-line)}iargs = <ESC>va{o^", {desc="Convert lines to dict", buffer =true})
 	end,
 })
 
--- dot_config/nvim/lua/plugins/highlight-colors.lua
+++ dot_config/nvim/lua/plugins/highlight-colors.lua
@@ -4,10 +4,10 @@ return {
 	"brenoprata10/nvim-highlight-colors",
 	event = "VeryLazy",
 	opts = {
-		render = "virtual",
-		virtual_symbol = "[]",
-		virtual_symbol_position = "eow",
-		virtual_symbol_prefix = " ",
-		virtual_symbol_suffix = "",
+		-- render = "virtual",
+		-- virtual_symbol = "[]",
+		-- virtual_symbol_position = "eow",
+		-- virtual_symbol_prefix = " ",
+		-- virtual_symbol_suffix = "",
 	},
 }
