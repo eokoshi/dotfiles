fb2db19f78e5692a6782a89082c0926c1b72af77
eokoshi <127910259+eokoshi@users.noreply.github.com>
2025-09-25 18:22:15 +0900

    Add .config/nvim/lua/plugins/bufferline.lua Add .config/nvim/lua/stuff/lualine_utils.lua

---
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,27 @@
+return {
+	"akinsho/bufferline.nvim",
+	event = "VeryLazy",
+	version = "*",
+	opts = {
+		options = {
+			themable = true,
+			-- separator_style = "slope",
+			diagnostics = "nvim_lsp",
+			show_tab_indicators = true,
+			offsets = {
+				{
+					filetype = "neo-tree",
+					text = "",
+					highlight = "BufferLineTab",
+					separator = true,
+				},
+			},
+			show_buffer_close_icons = false,
+			tab_size = 10,
+		},
+	},
+	init = function()
+		local map = require("stuff.functions").map
+		map("n", "<leader>bb", "<CMD>BufferLinePick<CR>", { desc = "Pick buffer" })
+	end,
+}
