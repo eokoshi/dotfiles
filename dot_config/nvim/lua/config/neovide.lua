3887e9f037589219793274450685448761aa6bb3
eokoshi <127910259+eokoshi@users.noreply.github.com>
2025-10-02 18:13:51 +0900

    Update .config/nvim/lua/config/neovide.lua Update .config/nvim/lua/plugins/fff.lua

--- dot_config/nvim/lua/config/neovide.lua
+++ dot_config/nvim/lua/config/neovide.lua
@@ -1,9 +1,10 @@
 -- Title bar color
 vim.g.neovide_title_background_color =
-	string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("FloatShadow") }).bg)
+	string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("BufferLineFill") }).bg)
 
 vim.o.guifont = "Cascadia_Mono_NF:h10"
-vim.g.neovide_title_text_color = "darkgrey"
+vim.g.neovide_title_text_color =
+	string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("BufferLineFill") }).fg)
 vim.g.neovide_floating_shadow = false
 
 if next(_G.arg) == nil then
