221a850eacc4c4949b5619bf9861f524f164a269
eokoshi <127910259+eokoshi@users.noreply.github.com>
2025-10-11 13:38:22 +0900

    Update .config/nvim/lua/plugins/checkmate.lua

--- dot_config/nvim/lua/plugins/checkmate.lua
+++ dot_config/nvim/lua/plugins/checkmate.lua
@@ -3,5 +3,17 @@ return {
 	ft = "markdown",
 	opts = {
 		files = { "*.md" },
+		metadata = {
+			started = {
+				get_value = function()
+					return tostring(os.date("%Y-%m-%d %H:%M"))
+				end,
+			},
+		},
+		todo_states = {
+			checked = {
+				marker = "󰸞",
+			},
+		},
 	},
 }
