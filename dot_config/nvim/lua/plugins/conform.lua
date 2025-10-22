62950f78f02fb791d8d0acde966fe98301751d65
eokoshi <127910259+eokoshi@users.noreply.github.com>
2025-10-22 10:56:59 +0900

    Update .config/nvim/lua/plugins/conform.lua

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,27 +1,61 @@
 return {
 	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>lf",
+			function()
+				require("conform").format({ async = true })
+			end,
+			mode = "n",
+			desc = "format buffer",
+		},
+		{
+			"<leader>bf",
+			function()
+				require("conform").format({ async = true })
+			end,
+			mode = "n",
+			desc = "format buffer",
+		},
+	},
+	---@module "conform"
+	---@type conform.setupOpts
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "ruff" },
 			r = { "air" },
 		},
-		format_on_save = {
-			-- These options will be passed to conform.format()
-			timeout_ms = 500,
+		default_format_opts = {
 			lsp_format = "fallback",
+		},
+		format_on_save = {
+			timeout_ms = 500,
 		},
 	},
 	init = function()
 		local map = require("stuff.functions").map
-		local conform = require("conform")
-		map("n", "<Leader>lc", "<CMD>ConformInfo<CR>")
-		map("n", "<Leader>bf", function()
-			conform.format()
-		end, { desc = "format buffer" })
-		map("n", "<Leader>lf", function()
-			conform.format()
-		end, { desc = "format buffer" })
+		map("n", "<Leader>lc", "<CMD>ConformInfo<CR>", { desc = "Formatter info" })
 		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
 	end,
 }
