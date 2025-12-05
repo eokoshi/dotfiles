a0fb70bcff6922dedda6cccba9e2cce1deed8cde
eokoshi <127910259+eokoshi@users.noreply.github.com>
2025-12-05 16:52:41 +0900

    Update .luarc.json Update .config/nvim/lua/options.lua Update .config/nvim/lua/plugins/mason.lua Update .config/nvim/lua/plugins/obsidian.lua Update .config/nvim/lua/plugins/treesitter.lua

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -85,7 +85,7 @@ vim.o.wrap = false -- Disable line wrap
 if vim.fn.has("win32") == 1 then
 	vim.o.shell = "pwsh"
 	vim.o.shellcmdflag =
-		"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+		"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8"
 	vim.o.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
 	vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
 	vim.o.shellquote = ""
