8efc255c9f5204b2c86aae8cc661b86f63ba627a
eokoshi <127910259+eokoshi@users.noreply.github.com>
2026-04-01 11:47:08 +0900

    update
    update

--- dot_setup.sh
+++ dot_setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-{{- if eq .chezmoi.osRelease.id "ubuntu" }}
+ARCH=$(uname -m)
 set -ueo pipefail
+export PATH="$PATH:~/.local/bin"
 
 ## apt packages
 sudo apt-get -y --ignore-missing install ripgrep fd-find python3-venv npm direnv tmux lsd unzip
@@ -16,8 +17,8 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
 		&& sudo mkdir -p -m 755 /etc/apt/keyrings \
-			&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
-			&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+			&& out=$(mktemp) && wget -nv -O"$out" https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+			&& cat "$out" | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
 		&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
 		&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 		&& sudo apt update \
@@ -29,7 +30,7 @@ echo ""
 read -p "Install chezmoi? [y/n]" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
+	sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME"/.local/bin
 	chezmoi init --apply eokoshi
 fi
 
@@ -61,13 +62,21 @@ echo ""
 read -p "Install nvim? [y/n]" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+	case $ARCH in
+		x86_64)
+			NVIM_SYS="nvim-linux-x86_64"
+			;;
+		aarch64|arm64)
+			NVIM_SYS="nvim-linux-arm64"
+			;;
+		*)
+			echo "Unsupported architecture: $ARCH"
+			exit 1
+			;;
+	esac
+
 	sudo rm -rf /opt/nvim
 	sudo mkdir /opt/nvim
-{{- if eq .chezmoi.arch "amd64"}}
-	NVIM_SYS="nvim-linux-x86_64"
-{{- else if eq .chezmoi.arch "arm64"}}
-	NVIM_SYS="nvim-linux-arm64"
-{{- end}}
 	curl -LO "https://github.com/neovim/neovim/releases/latest/download/${NVIM_SYS}.tar.gz"
 	sudo tar -C /opt/nvim -xzf "${NVIM_SYS}.tar.gz" --strip-components=1
 	rm -f "${NVIM_SYS}.tar.gz"
@@ -79,13 +88,21 @@ echo ""
 read -p "Install yazi? [y/n]" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+	case $ARCH in
+		x86_64)
+			YAZI_SYS="yazi-x86_64-unknown-linux-gnu"
+			;;
+		aarch64|arm64)
+			YAZI_SYS="yazi-aarch64-unknown-linux-gnu"
+			;;
+		*)
+			echo "Unsupported architecture: $ARCH"
+			exit 1
+			;;
+	esac
+
 	sudo rm -rf /opt/yazi
 	sudo mkdir /opt/yazi
-{{- if eq .chezmoi.arch "amd64"}}
-	YAZI_SYS="yazi-x86_64-unknown-linux-gnu"
-{{- else if eq .chezmoi.arch "arm64"}}
-	YAZI_SYS="yazi-aarch64-unknown-linux-gnu"
-{{- end}}
 	curl -LO "https://github.com/sxyazi/yazi/releases/latest/download/${YAZI_SYS}.zip"
 	unzip "${YAZI_SYS}.zip"
 	sudo mv "${YAZI_SYS}"/* /opt/yazi/
@@ -101,8 +118,27 @@ echo ""
 read -p "Install lazygit? [y/n]" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
+	case $ARCH in
+    x86_64)
+        LAZYGIT_ARCH="linux_x86_64"
+        ;;
+    aarch64|arm64)
+        LAZYGIT_ARCH="linux_arm64"
+        ;;
+    armv6l)
+        LAZYGIT_ARCH="linux_armv6"
+        ;;
+    i386|i686)
+        LAZYGIT_ARCH="linux_32-bit"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+	esac
+
+	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_${LAZYGIT_ARCH}.tar.gz"
 	tar xf lazygit.tar.gz lazygit
 	sudo install lazygit -D -t /usr/local/bin/
 	sudo rm -rf lazygit
@@ -110,11 +146,3 @@ then
 	lazygit --version
 fi
 echo ""
-
-{{- end }}
-
-{{- if eq .chezmoi.osRelease.id "arch" }}
-
-# TODO: how much to set up manually vs here?
-
-{{- end}}
