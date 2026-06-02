014fda3968f5471c72720780495bea469d0c8e20
eokoshi <127910259+eokoshi@users.noreply.github.com>
2026-06-02 17:39:09 +0900

    Remove .config/btop/btop.conf Remove .config/btop/themes/.keep Update .config/nvim/lua/plugins/colorschemes.lua

--- dot_config/nvim/lua/plugins/colorschemes.lua
+++ dot_config/nvim/lua/plugins/colorschemes.lua
@@ -22,6 +22,7 @@ return {
 			vim.g.everforest_background = "medium"
 			vim.g.everforest_better_performance = 1
 			vim.g.everforest_enable_italic = 1
+			-- vim.g.everforest_transparent_background = 1
 		end,
 	},
 	{
