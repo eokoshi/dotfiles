782f276b1248f273d58e4c6cb25699c66a3c8ece
eokoshi <127910259+eokoshi@users.noreply.github.com>
2026-08-06 21:32:11 +0900

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -244,6 +244,7 @@ return {
 		{
 			"obsidian-nvim/obsidian.nvim",
 			version = "*",
+			cmd = "Obsidian",
 			enabled = vim.fn.has("win32") == 1,
 			---@module 'obsidian'
 			---@diagnostic disable-next-line: type-not-found
