1aab176f66f570f46d763e4673ea2d7159eaa0b9
eokoshi <127910259+eokoshi@users.noreply.github.com>
2026-08-14 03:22:47 +0900

    Add .config/nvim/lazy-init.lua

---
+++ dot_config/nvim/lazy-init.lua
@@ -0,0 +1,18 @@
+vim.g.mapleader = " "
+
+require("lazy-init")
+
+require("options")
+require("autocmds")
+require("mappings")
+require("highlights")
+
+vim.cmd.colorscheme("gruvbox-material")
+
+if vim.fn.has("win32") == 1 then
+	require("windows")
+elseif vim.fn.environ()["TERM"] == "linux" then
+	vim.cmd.colorscheme("default")
+end
+
+require("final")
