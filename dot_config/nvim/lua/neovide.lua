785fe645ec51aec4b61a363a3b655cc222a6adcc
eokoshi <127910259+eokoshi@users.noreply.github.com>
2026-09-16 18:27:59 +0900

    Update .config/nvim/lua/neovide.lua

--- dot_config/nvim/lua/neovide.lua
+++ dot_config/nvim/lua/neovide.lua
@@ -3,6 +3,12 @@ vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_floating_shadow = false
 vim.g.neovide_remember_window_size = false
 
+vim.g.neovide_scale_factor = 1.0
+local change_scale_factor = function(delta) vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta end
+vim.keymap.set("n", "<C-;>", function() change_scale_factor(1.25) end)
+vim.keymap.set("n", "<C-->", function() change_scale_factor(1 / 1.25) end)
+vim.keymap.set("n", "<C-0>", function() vim.g.neovide_scale_factor = 1 end)
+
 local neovide_augroup = vim.api.nvim_create_augroup("neovide", { clear = true })
 vim.api.nvim_create_autocmd("ColorScheme", {
 	group = neovide_augroup,
